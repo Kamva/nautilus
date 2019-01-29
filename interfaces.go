0eb733cbcb98947665eb7f30150310557f5538bc
Behzad Shabani <behzad.shabani@gmail.com>
2019-01-29 12:07:07 +0330

    Add BaseTaggable

--- interfaces.go
+++ interfaces.go
@@ -1,6 +1,18 @@
 package nautilus
 
+import "reflect"
+
 // Taggable is an interface for struct that have tagging
 type Taggable interface {
 	GetTag(caller interface{}, field string, tag string) string
+}
+
+// BaseTaggable is a base struct for helper for struct needed to implement
+// Taggable interface
+type BaseTaggable struct{}
+
+// GetTag Get `tag` value on the `field` of `caller`
+func (r BaseTaggable) GetTag(caller interface{}, field string, tag string) string {
+	taggedField, _ := reflect.TypeOf(caller).Elem().FieldByName(field)
+	return taggedField.Tag.Get(tag)
 }
